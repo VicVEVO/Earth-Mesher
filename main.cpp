368875840088a82810c3a7e1eedc135a46d335f6
vic_pabo <viclouisnoe@gmail.com>
2024-02-01 23:59:05 +0100

    v0.01

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include "submodules/libigl/include/igl/cotmatrix.h"
+#include "submodules/eigen/Eigen/Dense"
+#include "submodules/eigen/Eigen/Sparse"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    Eigen::MatrixXd V(4,2);
+    V<<0,0,
+            1,0,
+            1,1,
+            0,1;
+    Eigen::MatrixXi F(2,3);
+    F<<0,1,2,
+            0,2,3;
+    Eigen::SparseMatrix<double> L;
+    igl::cotmatrix(V,F,L);
+    std::cout<<"Hello, mesh: "<<std::endl<<L*V<<std::endl;
     return 0;
 }
