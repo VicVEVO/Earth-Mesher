f3e5ca7aaaaf21a27f1f2fb67c56497d3a68c738
vic_pabo <viclouisnoe@gmail.com>
2024-02-12 18:30:48 +0100

    robustness v1

---
+++ errorCode.h
@@ -0,0 +1,8 @@
+//
+// Created by vic_pabo on 2/12/24.
+//
+
+#ifndef RENDER_ERRORCODE_H
+#define RENDER_ERRORCODE_H
+
+#endif //RENDER_ERRORCODE_H
---
+++ errorMessages.h
@@ -0,0 +1,8 @@
+//
+// Created by vic_pabo on 2/12/24.
+//
+
+#ifndef RENDER_ERRORMESSAGES_H
+#define RENDER_ERRORMESSAGES_H
+
+#endif //RENDER_ERRORMESSAGES_H
